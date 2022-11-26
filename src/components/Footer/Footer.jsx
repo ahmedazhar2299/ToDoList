e40c906e5ca10ababefadb1996f1da2084ef7061
Muhammad Ahmed <62472263+ahmedazhar2299@users.noreply.github.com>
2022-11-27 00:41:44 +0500

    v1.1 client

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer({length}) {
   return (
-    <div  className={`w-full flex bottom-0 ${length<5?"absolute":"relative"} top-auto justify-between px-10 py-5 bg-slate-800 text-white text-sm mt-4`}>
+    <div  className={`w-full flex bottom-0 ${length<=5?"absolute":"relative"} top-auto justify-between px-10 py-5 bg-slate-800 text-white text-sm mt-4`}>
       <div>
         <p>Copyright © 2022 | Assignment 3 WEB 7A</p>
       </div>
